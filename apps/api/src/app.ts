e9b96494236ede48c01414419669342437411621
rndall <96964503+rndall@users.noreply.github.com>
2026-03-15 16:08:23 +0800

    build(api): install compression and helmet

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,10 +1,15 @@
+import compression from "compression"
 import express, { type Express } from "express"
+import helmet from "helmet"
 
 import { errorHandler } from "@/middlewares/errorHandler"
 
 import postsRouter from "@/routes/posts"
 
 const app: Express = express()
+
+app.use(helmet())
+app.use(compression())
 
 app.use("/posts", postsRouter)
 
