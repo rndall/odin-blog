ec75748e0e6b7f629081479a39990089d720c126
rndall <96964503+rndall@users.noreply.github.com>
2026-03-16 15:41:28 +0800

    feat(api): create validate body middleware

--- apps/api/src/middlewares/validate.ts
+++ apps/api/src/middlewares/validate.ts
@@ -9,3 +9,12 @@ export const validateParams =
 		}
 		next()
 	}
+
+export const validateBody =
+	(schema: ZodType) => (req: Request, res: Response, next: NextFunction) => {
+		const result = schema.safeParse(req.body)
+		if (!result.success) {
+			return res.status(400).json(z.treeifyError(result.error))
+		}
+		next()
+	}
