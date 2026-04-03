1f8816fef658398fb6818566df3aade5e0ea9109
rndall <96964503+rndall@users.noreply.github.com>
2026-04-03 15:59:24 +0800

    feat(reader): create navbar

--- apps/reader/src/routes/+layout.svelte
+++ apps/reader/src/routes/+layout.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
-  import "./layout.css";
-  import favicon from "$lib/assets/favicon.svg";
+	import './layout.css'
+	import favicon from '$lib/assets/favicon.svg'
+	import Navbar from './navbar.svelte'
 
-  let { children } = $props();
+	let { children } = $props()
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+
+<Navbar />
+<main class="container">
+	{@render children()}
+</main>
---
+++ apps/reader/src/routes/navbar.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { Search01Icon } from '@hugeicons/core-free-icons'
+	import { HugeiconsIcon } from '@hugeicons/svelte'
+	import { resolve } from '$app/paths'
+	import { page } from '$app/state'
+	import { Button } from '$lib/components/ui/button'
+</script>
+
+<header class="container flex items-center justify-between bg-[#fefefe] py-4">
+	<a href={resolve('/')} class="my-2 font-serif text-3xl font-bold text-primary italic"
+		>The Digital Atelier</a
+	>
+	<nav>
+		<ul
+			class="flex font-serif **:text-base **:font-bold **:text-muted-foreground **:decoration-2 **:underline-offset-8 **:aria-current:text-primary **:aria-current:underline"
+		>
+			<li>
+				<Button aria-current={page.url.pathname === '/'} href={resolve('/')} variant="link"
+					>Essays</Button
+				>
+			</li>
+			<li>
+				<Button
+					aria-current={page.url.pathname === '/archive'}
+					href={resolve('/archive')}
+					variant="link">Archive</Button
+				>
+			</li>
+		</ul>
+	</nav>
+
+	<div>
+		<Button size="icon" variant="ghost" aria-label="search">
+			<HugeiconsIcon class="text-primary" strokeWidth={3} icon={Search01Icon} />
+		</Button>
+		<Button class="rounded-sm">Sign In</Button>
+	</div>
+</header>
