863ffc4db657361a91196a72f348bbfc94c0ae5d
rndall <96964503+rndall@users.noreply.github.com>
2026-04-03 14:52:14 +0800

    build(reader): install prettier

--- apps/reader/src/lib/utils.ts
+++ apps/reader/src/lib/utils.ts
@@ -1,5 +1,5 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
@@ -7,12 +7,10 @@ export function cn(...inputs: ClassValue[]) {
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 // biome-ignore lint/suspicious/noExplicitAny: used for generic type constraint checking only
-export type WithoutChild<T> = T extends { child?: any } ? Omit<T, "child"> : T
+export type WithoutChild<T> = T extends { child?: any } ? Omit<T, 'child'> : T
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 // biome-ignore lint/suspicious/noExplicitAny: any is required here to match any property value for Omit logic
-export type WithoutChildren<T> = T extends { children?: any }
-	? Omit<T, "children">
-	: T
+export type WithoutChildren<T> = T extends { children?: any } ? Omit<T, 'children'> : T
 export type WithoutChildrenOrChild<T> = WithoutChildren<WithoutChild<T>>
 export type WithElementRef<T, U extends HTMLElement = HTMLElement> = T & {
 	ref?: U | null
